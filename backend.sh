57b2df7da5e654a2893b431dfa6a5a81af359425
jahnavi <awsdevopsjanu555@gmail.com>
2024-11-14 12:19:07 +0530

    backend

---
+++ backend.sh
@@ -0,0 +1,54 @@
+ #!/bin/bash
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+dnf module disable nodejs -y &>>LOG_FILE #redirecting logs to log file
+VALIDATE $? "disable default nodejs"
+
+dnf module enable nodejs:20 -y &>>LOG_FILE
+VALIDATE $? "enabling nodejs:20"
+
+dnf install nodejs -y &>>LOG_FILE
+VALIDATE $? "Install nodejs"
+
+#userid will be craeted when we run the command on terminal which creates IDEMPOTENCY
+#so we check in the if condition
+#id expense , if this command has userid it will return O so userid will not be craeted again...if id expense has no such user , this will create expense user
+id expense &>>LOG_FILE
+if [ $? -ne 0 ]
+then 
+echo -e " $Y creating user $N " &>>LOG_FILE
+useradd expense 
+VALIDATE $? "Creating expense user"
+else
+echo "user is already craeted ..please check.. $R skipping $N" &>>LOG_FILE
+fi
