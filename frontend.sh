8edeea81e4b5c24572651c9c2f948cf5fb3a30fe
jahnavi <awsdevopsjanu555@gmail.com>
2024-11-14 13:49:58 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-R="\e[31m"  #to print red colour
-G="\e[32m"  #to print green  colour
-Y="\e[33m"  #to print yellow colour
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 
-LOGS_FOLDER="/var/log/expense"  #to store logs in this expense directory
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #this will return the folder name 
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S) #this is to craete a timestamp
-LOG_FILE="$LOGS_FOLDRE/$SCRIPT_NAME-$TIMESTAMMP.log"  #logfloder , scriptname,timestamp are together
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
-mkdir -p $LOGS_FOLDER #this is to create log folder  -p : is used if this already a director with same name then it will not craete if there is no directory with this name then directory is created
-
-USERID=$(id -u) #will give the used id id it is sudo then O or 1001 /1002..
-
-CHECK_ROOT(){    #this function is to check whether command is run with root access or not , if not exit
-   
+CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
         echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
@@ -24,40 +22,39 @@ CHECK_ROOT(){    #this function is to check whether command is run with root acc
 
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-       echo -e "$2 is $R FALIED $N" | tee -a $LOG_FILE  #tee command is used to prit log in terminal and in log folder
-    exit 1
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
     else
-        echo -e "$2 is $R SUCCESS $N" &>> $LOG_FILE
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
-
 }
 
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
 CHECK_ROOT
-dnf install nginx -y &>>LOG_FILE
+
+dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx  &>>LOG_FILE
+systemctl enable nginx &>>$LOG_FILE
 VALIDATE $? "Enable Nginx"
 
-systemctl start nginx &>>LOG_FILE
-VALIDATE $? "start Nginx"
+systemctl start nginx &>>$LOG_FILE
+VALIDATE $? "Start Nginx"
 
-rm -rf /usr/share/nginx/html/* &>>LOG_FILE  #by default when we run ip address we get red hat page , we will be removing that default page 
-VALIDATE $? "removing default website"
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "Removing default website"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
 VALIDATE $? "Downloding frontend code"
 
-cd /usr/share/nginx/html  #we will be going into that directory and downloag
-
-unzip /tmp/frontend.zip &>>$LOG_FILE  #extract the frontend code
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extract frontend code"
 
-cp /home/ec2-user/expense-project/expense.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restarted Nginx"
-
